c21a699570b468351fd3b3d659f026675ddf1913
jota <juan.erices04@inacapmail.cl>
2024-11-29 18:50:55 -0300

    Add multiple entries to registro_herramientas.txt for tool selection logs

--- main.py
+++ main.py
@@ -5,122 +5,178 @@ import cv2
 import face_recognition
 import os
 from datetime import datetime # type: ignore
+from PIL import Image, ImageTk
 
-class sistema_panol:
-    def __init__(self, root):
+# Paths
+icon_path = "img/icono.ico"
+reconociendo_path = "img/reconociendo_rostro.png"
+
+class VentanaInicio:
+    def __init__(self, root, codificaciones):
         self.root = root
-        self.root.title("Sistema de Reconocimiento Facial")
-        self.root.attributes('-fullscreen',True)
-        self.root.state("zoomed")
-        self.root.resizable(False, False)
-        self.dataset_dir = "Dataset"
-        self.codificaciones = self.cargar_imagenes_alumnos()
-        self.inicializar_ventana_principal()
+        self.codificaciones = codificaciones
+        self.frame = ttk.Frame(self.root, padding=50)
+        self.frame.pack(expand=True)
 
-    # Ventana 1: Inicio -------------------------------------
-    def inicializar_ventana_principal(self):
-        frame = ttk.Frame(self.root, padding=50)
-        frame.pack(expand=True)
+        # Boton Iniciar
+        btn_iniciar = ttk.Button(
+            self.frame,
+            text="Iniciar",
+            command=self.abrir_ventana_camara
+        )
+        btn_iniciar.pack(padx=5, pady=5)
+        btn_iniciar.config(
+            width=15,
+            style="Tactil.TButton"
+        )
+        estilo = ttk.Style()
+        estilo.configure(
+            "Tactil.TButton",
+            font=("Arial", 10),
+            padding=10
+        )
 
-        btn_iniciar = ttk.Button(frame, text="Iniciar", command=self.abrir_ventana_camara)
-        btn_iniciar.pack()
-
-    # Ventana 2: Camara -------------------------------------
     def abrir_ventana_camara(self):
         self.root.withdraw()
-        cam_window = tk.Toplevel(self.root)
-        cam_window.title("Camara")
-        cam_window.attributes('-fullscreen',True)
-        cam_window.state("zoomed")
-        cam_window.configure(bg="white")
+        VentanaCamara(self.root, self.codificaciones)
 
-        lbl_status = ttk.Label(cam_window, text="Detectando rostro...", background="white", font=("Arial", 16))
-        lbl_status.pack(pady=20)
 
-        def iniciar_camara():
-            cap = cv2.VideoCapture(0)
-            reconocida = False
-            while True:
-                ret, frame = cap.read()
-                if not ret:
+class VentanaCamara:
+    def __init__(self, root, codificaciones):
+        self.root = root
+        self.codificaciones = codificaciones
+        self.cam_window = tk.Toplevel(self.root)
+        self.cam_window.title("Cámara")
+        self.cam_window.attributes('-fullscreen', True)
+        self.cam_window.state("zoomed")
+        self.cam_window.configure(bg="white")
+
+        self.lbl_status = ttk.Label(self.cam_window, text="Detectando rostro...", background="white", font=("Arial", 16))
+        self.lbl_status.pack(pady=20)
+        
+        imagen_original = Image.open(reconociendo_path)
+        imagen_redimensionada = imagen_original.resize((800, 800))
+        self.imagen_tk = ImageTk.PhotoImage(imagen_redimensionada)  # Guardar como atributo de la clase
+
+        self.label_imagen = ttk.Label(self.cam_window, image=self.imagen_tk)
+        self.label_imagen.pack(pady=(20, 10))
+
+        #100 ms
+        self.cam_window.after(100, self.iniciar_camara)
+
+    def iniciar_camara(self):
+        cap = cv2.VideoCapture(0)
+        reconocida = False
+        while True:
+            ret, frame = cap.read()
+            if not ret:
+                break
+            frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            rostros = face_recognition.face_locations(frame_rgb)
+            codificaciones_rostros = face_recognition.face_encodings(frame_rgb, rostros)
+
+            for codificacion in codificaciones_rostros:
+                for nombre, referencia in self.codificaciones.items():
+                    if face_recognition.compare_faces([referencia], codificacion)[0]:
+                        self.lbl_status.config(text=f"Reconocido: {nombre}")
+                        reconocida = True
+                        cap.release()
+                        cv2.destroyAllWindows()
+                        self.cam_window.destroy()
+                        VentanaHerramientas(self.root, nombre)
+                        return
+
+            if not reconocida:
+                self.lbl_status.config(text="No se reconoce a la persona")
+                cv2.imshow("Cámara", frame)
+                if cv2.waitKey(1) & 0xFF == ord("q"):
                     break
-                frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-                rostros = face_recognition.face_locations(frame_rgb)
-                codificaciones_rostros = face_recognition.face_encodings(frame_rgb, rostros)
+        cap.release()
+        cv2.destroyAllWindows()
+        self.cam_window.destroy()
+        self.root.deiconify()
 
-                for codificacion in codificaciones_rostros:
-                    for nombre, referencia in self.codificaciones.items():
-                        if face_recognition.compare_faces([referencia], codificacion)[0]:
-                            lbl_status.config(text=f"Reconocido: {nombre}")
-                            reconocida = True
-                            cap.release()
-                            cv2.destroyAllWindows()
-                            cam_window.destroy()
-                            self.abrir_ventana_herramientas(nombre)
-                            return
 
-                if not reconocida:
-                    lbl_status.config(text="No se reconoce a la persona")
-                    cv2.imshow("Cámara", frame)
-                    if cv2.waitKey(1) & 0xFF == ord("q"):
-                        break
-            cap.release()
-            cv2.destroyAllWindows()
-            cam_window.destroy()
-            self.root.deiconify()
+class VentanaHerramientas:
+    def __init__(self, root, nombre):
+        self.root = root
+        self.nombre = nombre
+        self.tool_window = tk.Toplevel(self.root)
+        self.tool_window.title("Ventana de Herramientas")
+        self.tool_window.geometry("1920x1080")
+        self.tool_window.iconbitmap(icon_path)
+        self.tool_window.attributes('-fullscreen', True)
+        self.tool_window.state("zoomed")
 
-        cam_window.after(100, iniciar_camara)
+        self.frame = ttk.Frame(self.tool_window, padding=50)
+        self.frame.pack(expand=True)
 
-    # Ventana 3: Herramientas -------------------------------------
-    def abrir_ventana_herramientas(self, nombre):
-        tool_window = tk.Toplevel(self.root)
-        tool_window.title("Ventana de Herramientas")
-        tool_window.geometry("1920x1080")
-        # tool_window.attributes('-fullscreen',True)
-        tool_window.state("zoomed")
-        frame = ttk.Frame(tool_window, padding=50)
-        frame.pack(expand=True)
-
-        ttk.Label(frame, text=f"Bienvenido, {nombre}", font=("Arial", 16)).pack(pady=10)
+        # Bienvenido
+        ttk.Label(
+            self.frame,
+            text=f"Bienvenido, {self.nombre}",
+            font=("Arial", 15)  # Aumentar el tamaño de la fuente
+        ).pack(pady=10)
 
         # Lista de herramientas
         herramientas = ["Martillo", "Destornillador", "Llave inglesa", "Taladro", "Sierra"]
-        # Variables para checkboxes
-        seleccion_herramientas = {}  # Diccionario para almacenar las variables
+        self.seleccion_herramientas = {}
 
-        # Checkboxes
-        for herramienta in herramientas:
-            var = tk.BooleanVar(value=False)  # Crear una variable para el estado del checkbox
-            seleccion_herramientas[herramienta] = var  # Guardar la variable en el diccionario
-            checkbox = ttk.Checkbutton(frame, text=herramienta, variable=var)  # Vincular el checkbox con la variable
-            checkbox.pack(anchor="w", padx=20)
-
-        def guardar_seleccion():
-            # Obtener herramientas
-            herramientas_seleccionadas = [
-                herramienta for herramienta, var in seleccion_herramientas.items() if var.get()
-            ]
-            # Imprimir
-            fecha_hora = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-            print(f"Usuario: {nombre}\nFecha y hora: {fecha_hora}\nHerramientas seleccionadas: {herramientas_seleccionadas}")
-
-            # Guardar herramientas en 'registro_herramientas.txt'
-            with open("registro_herramientas.txt", "a", encoding="utf-8") as file:
-                file.write(f"\nUsuario: {nombre}\n")
-                file.write(f"Fecha y hora: {fecha_hora}\n")
-                file.write(f"Herramientas seleccionadas: {', '.join(herramientas_seleccionadas)}\n")
-                file.write("-" * 50 + "\n")
-
-            messagebox.showinfo("Confirmación", "Selección guardada correctamente.")
-            tool_window.destroy()
-            self.root.deiconify()  # Volver a la Ventana 1
-
-        btn_guardar = ttk.Button(
-            frame,
-            text="Listo",
-            command=guardar_seleccion
+        estilo = ttk.Style()
+        estilo.configure(
+            "Tactil.TCheckbutton",
+            font=("Arial", 15),  # Fuente más grande para los checkboxes
+            padding=5  # Relleno interno
         )
-        btn_guardar.pack(pady=20)
+
+        for herramienta in herramientas:
+            var = tk.BooleanVar(value=False)
+            self.seleccion_herramientas[herramienta] = var
+            checkbox = ttk.Checkbutton(
+                self.frame,
+                text=herramienta,
+                variable=var,
+                style="Tactil.TCheckbutton"
+            )
+            checkbox.pack(anchor="w", padx=10, pady=5)  # Espaciado externo para mayor comodidad
+
+        # Boton Listo
+        btn_guardar = ttk.Button(
+            self.frame,
+            text="Listo",
+            command=self.guardar_seleccion,
+            style="Tactil.TButton"
+        )
+        btn_guardar.pack(pady=10)
+
+    def guardar_seleccion(self):
+        herramientas_seleccionadas = [
+            herramienta for herramienta, var in self.seleccion_herramientas.items() if var.get()
+        ]
+        fecha_hora = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        with open("registro_herramientas.txt", "a", encoding="utf-8") as file:
+            file.write(f"\nUsuario: {self.nombre}\n")
+            file.write(f"Fecha y hora: {fecha_hora}\n")
+            file.write(f"Herramientas seleccionadas: {', '.join(herramientas_seleccionadas)}\n")
+            file.write("-" * 50 + "\n")
+
+        messagebox.showinfo("Confirmación", "Selección guardada correctamente.")
+        self.tool_window.destroy()
+        self.root.deiconify()
+
+
+class SistemaPanol:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Sistema de Reconocimiento Facial")
+        self.root.attributes('-fullscreen', True)
+        self.root.state("zoomed")
+        self.root.resizable(False, False)
+        self.root.iconbitmap(icon_path)
+        self.dataset_dir = "Dataset"
+        self.codificaciones = self.cargar_imagenes_alumnos()
+
+        VentanaInicio(self.root, self.codificaciones)
 
     def cargar_imagenes_alumnos(self):
         codificaciones = {}
@@ -140,5 +196,5 @@ class sistema_panol:
 
 if __name__ == "__main__":
     root = ThemedTk(theme="arc")
-    app = sistema_panol(root)
+    app = SistemaPanol(root)
     root.mainloop()
